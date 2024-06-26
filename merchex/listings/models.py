2448f57659e9e017ff4816acbf959e08396ef574
Valboulet <valboulet59@gmail.com>
2024-06-26 14:32:39 +0200

    Migrated new columns in database

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,34 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2024)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Listing(models.Model):
+
+    class Type(models.TextChoices):
+        RECORDS = 'R'
+        CLOTHING = 'C'
+        POSTERS = 'P'
+        MISCELLANEOUS = 'M'
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=200)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+        validators=[MinValueValidator(1960), MaxValueValidator(2024)], null=True
+    )
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
